ae73e15938124dec8168073869dd2ac8a030438b
Russell Gould <gouldru@oregonstate.edu>
2020-10-16 14:55:01 -0700

    ensure consisteny between notebook and main.py

--- src/main.py
+++ src/main.py
@@ -8,30 +8,21 @@ import pandas as pd
 from sklearn import linear_model, metrics, model_selection
 from sys import argv
 
-results_dir = argv[1]
+dat_in_pth = "../data/features.pkl"
+dat_df = pd.read_pickle(dat_in_pth)
 
-dat_in_pth = results_dir + "/features.pkl"
-dat_df = pd.read_pickle(dat_in_pth).set_index(keys="index")
-
-neg_to_pos_ratio = str(
-    round(
-        dat_df[dat_df["class"] == 0].shape[0] / dat_df[dat_df["class"] == 1].shape[0],
-        ndigits=1,
-    )
-)
-
+results_dir = "results"
 seed = 8675309
 np.random.seed(seed)
 
-seed_out_pth = results_dir + "/" + str(seed)
-if not os.path.exists(seed_out_pth):
-    os.makedirs(seed_out_pth)
+if not os.path.exists(results_dir):
+    os.makedirs(results_dir)
 
-training_set_tss_ids_pth = seed_out_pth + "/training_set_tss_ids.txt"
-heldout_test_set_tss_ids_pth = seed_out_pth + "/heldout_test_set_tss_ids.txt"
-crossval_results_pth = seed_out_pth + "/crossval_results.csv"
-heldout_test_performance_pth = seed_out_pth + "/heldout_test_performance.csv"
-factors_pth = seed_out_pth + "/factors.csv"
+training_set_tss_ids_pth = results_dir + "/training_set_tss_ids.txt"
+heldout_test_set_tss_ids_pth = results_dir + "/heldout_test_set_tss_ids.txt"
+crossval_results_pth = results_dir + "/crossval_results.csv"
+test_performance_pth = results_dir + "/heldout_test_performance.csv"
+coefs_pth = results_dir + "/coefficients.csv"
 
 ###########################################################################
 # split data into 80/20 train/test
@@ -44,12 +35,14 @@ np.savetxt(heldout_test_set_tss_ids_pth, heldout_test.index.values, fmt="%s")
 
 ###########################################################################
 # define log reg cv object
+n_procs = 10
+
 log_cv = linear_model.LogisticRegressionCV(
     solver="liblinear",
     Cs=np.logspace(-4, 4, 100),
     cv=5,
     penalty="l1",
-    n_jobs=int(multiprocessing.cpu_count() / 2),
+    n_jobs=n_procs,
     refit=True,
     scoring="average_precision",
     random_state=seed,
@@ -74,7 +67,6 @@ roc_curve_df = (
     .reset_index(drop=True)
 )
 roc_curve_df["Y"] = roc_curve_df["TPR"] - roc_curve_df["FPR"]
-roc_curve_df = roc_curve_df.round(4)
 youden_T = roc_curve_df.iloc[roc_curve_df["Y"].idxmax()]["threshold"]
 
 y_pred_youden = np.where(y_prediction_probabilities[:, 1] >= youden_T, 1, 0)
@@ -99,28 +91,24 @@ PPV = TP / (TP + FP)
 
 heldout_perf_f1 = 2 * ((PPV * TPR) / (PPV + TPR))
 
-metrics_dict = {
-    "C": log_cv.C_[0],
-    "auROC": heldout_perf_roc,
-    "auPRC": heldout_perf_prc,
-    "youden_T": youden_T,
-    "sensitivity": TPR,
-    "specificity": TNR,
-    "precision": PPV,
-    "f1": heldout_perf_f1,
-    "seed": seed,
-    "ratio": neg_to_pos_ratio,
-}
+perf_metrics = pd.Series(
+    {
+        "C": log_cv.C_[0],
+        "auROC": heldout_perf_roc,
+        "auPRC": heldout_perf_prc,
+        "youden_T": youden_T,
+        "sensitivity": TPR,
+        "specificity": TNR,
+        "precision": PPV,
+        "f1": heldout_perf_f1,
+        "seed": seed,
+    }
+)
 
-# save metrics as csv
-metrics_df = pd.DataFrame(
-    metrics_dict.values(), index=metrics_dict.keys(), dtype=float
-).T
-metrics_df["seed"] = metrics_df["seed"].astype(int)
-metrics_df.to_csv(heldout_test_performance_pth, header=True)
+coefficients = pd.DataFrame(log_cv.coef_, columns=dat_df.columns[:-1])
+
+# save metrics
+perf_metrics.to_csv(test_performance_pth, header=True)
 
 # save coefficient values
-factors_df = pd.DataFrame(log_cv.coef_, columns=dat_df.columns[:-1])
-factors_df["seed"] = seed
-factors_df["ratio"] = neg_to_pos_ratio
-factors_df.to_csv(factors_pth, header=True)
+coefficients.to_csv(coefs_pth, header=True)
